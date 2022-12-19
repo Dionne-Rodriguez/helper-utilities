f3b7a2200b9e4543218d2feeab5fd76205966890
Dionne <dionnerodriguez.se@gmail.com>
2022-12-18 21:15:20 -0500

    finish overspeed integration for mirage 2000

--- Speed-Warning.js
+++ Speed-Warning.js
@@ -2,28 +2,42 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+  });
   const page = await browser.newPage();
-  await page.goto('https://www.example.com/');
+  await page.goto('http://127.0.0.1:8111/');
 
-  await page.exposeFunction('puppeteerLogMutation', () => {
-    console.log('Mutation Detected: A child node has been added or removed.');
+  const audioObj = await page.evaluateHandle(() => {
+    const path = "http://127.0.0.1:8080/sounds/overspeed-warning-plane-By-Tuna.mp3"
+    const audio = new Audio(path)
+    return audio
   });
 
-  await page.evaluate(() => {
-    const target = document.querySelector('body');
-    const observer = new MutationObserver( mutations => {
-      for (const mutation of mutations) {
-        if (mutation.type === 'childList') {
-          puppeteerLogMutation();
+  await page.evaluate(async (audio) => {
+    const target = document.querySelector('#state0 > li:nth-child(2)');
+    const observer = new MutationObserver(async mutations => {
+      for await (const mutation of mutations) {
+        if (mutation.type === 'childList' && parseNumber(document.querySelector('#state0 > li:nth-child(3)').innerHTML) > 1340) {
+          audio.play()
+          console.log(parseNumber(document.querySelector('#state0 > li:nth-child(2)').innerHTML), "speed");
+        } else {
+          audio.pause()
         }
       }
     });
     observer.observe(target, { childList: true });
-  });
 
-  await page.evaluate(() => {
-    document.querySelector('body').appendChild(document.createElement('br'));
-  });
-
+    function parseNumber(string) {
+      var regex = /\d*\.?\d+(?:e-?\d+)?$/;
+      var match = string.match(regex);
+      if (match) {
+        var numberString = match[0].replace(/,/g, "");
+        return parseFloat(numberString);
+      } else {
+        return NaN;
+      }
+    }
+  }, audioObj);
 })();
