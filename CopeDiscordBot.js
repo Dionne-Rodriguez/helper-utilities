ae36951133c7bdc02efcf5a68e954b6353f44e4b
Dionne <dionnerodriguez.se@gmail.com>
2023-03-10 19:59:51 -0500

    implement entire team point tracking

--- CopeDiscordBot.js
+++ CopeDiscordBot.js
@@ -1,18 +1,29 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js'
-// Create a new client instance
+import dotenv from 'dotenv'
+dotenv.config()
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const token =  process.env.TOKEN
+const token = process.env.TOKEN
 const channelId = process.env.CHANNELID
 
-
-
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+    console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.login(token);
 
 export default function sendMessage(message) {
     const channel = client.channels.cache.get(channelId);
     channel.send(message);
 }
+
+client.login(token);
+
+
+
+
+//discordbot will wait once a team is created
+//a created team contains 8 members that are in game names
+//once a team is created, script will call apropriate tracker file to track
+
+
+
+
