f281be5072ac069ad6f56de6d3811e4b49bac9fd
Dionne <dionne.r.9@gmail.com>
2023-03-31 20:43:30 -0400

    finish top ten tracker

--- trackTopTen.js
+++ trackTopTen.js
@@ -11,7 +11,7 @@ import cron from "node-cron";
 //   stopScraping();
 // });
 
-var intervalId, initialTopTenSquadPoints, endSessionStats;
+var intervalId, initialTopTenSquadPoints, latestTopTenSquadPoints;
 const startScraping = async () => {
     console.log("Start scraping at:", new Date().toLocaleTimeString("en-US"));
 
@@ -35,8 +35,6 @@ const startScraping = async () => {
             }
             return topTenTeams;
         });
-    
-
         topTenTeamPoints = new Map(topTenTeamPoints);
 
         for (const [teamName, teamLink] of topTenTeamPoints.entries()) {
@@ -48,18 +46,14 @@ const startScraping = async () => {
                     document.querySelector(".squadrons-counter__value").innerHTML.trim()
                 );
             });
-            console.log(squadronPoints);
             topTenTeamPoints.set(teamName, squadronPoints);
         }
-
         return topTenTeamPoints
-   
     };
 
 
-    initialTopTenSquadronPoints()
     await updateGlobalVariables();
-    // console.log("Initial value:", initialSessionStats);
+    console.log("Initial value:", initialTopTenSquadPoints);
 
     async function getUpdatedSquadronStats() {
         const browser = await puppeteer.launch({
@@ -71,76 +65,57 @@ const startScraping = async () => {
         await page.setDefaultNavigationTimeout(0);
         await page.goto("https://warthunder.com/en/community/clansleaderboard/");
 
-        var topTenTeamNames = new Map();
-        topTenTeamNames.set({ names: 0 });
-
-        var topTenTeamNames = await page.evaluate(() => {
-            for (let i = 0; i <= 10; i++) {
-                const names = document
-                    .querySelectorAll("tr")
-                [i].childNodes.map((el) => el.innerText);
+        var topTenTeamPoints = await page.evaluate(() => {
+            var topTenTeams = []
+            for (let i = 1; i < 11; i++) {
+                topTenTeams.push([
+                    document.querySelectorAll("tr")[i].childNodes[1].innerText.split(" ")[0],
+                    document.querySelectorAll("tr")[i].childNodes[1].firstChild.href
+                ]);
             }
-            return names;
+            return topTenTeams;
         });
+        topTenTeamPoints = new Map(topTenTeamPoints);
 
-        console.log("Updated value:", topTenTeamNames);
+        for (const [teamName, teamLink] of topTenTeamPoints.entries()) {
+            await page.setDefaultNavigationTimeout(0);
+            await page.goto(teamLink);
+        
+            var squadronPoints = await page.evaluate(() => {
+                return parseInt(
+                    document.querySelector(".squadrons-counter__value").innerHTML.trim()
+                );
+            });
+            topTenTeamPoints.set(teamName, squadronPoints);
+        }
+
+        console.log("Updated value:", topTenTeamPoints);
         await browser.close();
 
-        if (squadronPoints > initialTopTenSquadPoints.squadronPoints) {
-            var pointsGained = squadronPoints - initialTopTenSquadPoints.squadronPoints;
-            var { killGain, deathGain, kdRatio, calculatedRank } = calculateStats();
-            sendMessage(`#${calculatedRank()} F4WRD 🇺🇸🦅
-  Points: ${squadronPoints} (<:smallgreenuptriangle:1083528485890445342> +${pointsGained})
-  Kills: ${kills} (+${killGain})
-  Deaths: ${deaths} (+ ${deathGain})
-  K/D: ${kdRatio}
-  Players: ${squadronPlayerCount}
-  `);
+        let changesMessage = "Squadron points changes detected:\n";
+        let rank = 1;
+    
+        for (const [teamName, initialPoints] of initialTopTenSquadPoints.entries()) {
+            const updatedPoints = topTenTeamPoints.get(teamName);
+            const pointsDifference = updatedPoints - initialPoints;
+            const differenceSymbol = pointsDifference > 0 ? "<:smallgreenuptriangle:1083528485890445342>" : ":small_red_triangle_down:";
+            changesMessage += `${rank}. ${teamName}: ${initialPoints} -> ${updatedPoints} (${differenceSymbol} ${pointsDifference})\n`;
+            rank++;
         }
-        if (squadronPoints < initialTopTenSquadPoints.squadronPoints) {
-            var pointsLost = initialTopTenSquadPoints.squadronPoints - squadronPoints;
-            var { killGain, deathGain, kdRatio, calculatedRank } = calculateStats();
-            sendMessage(`#${calculatedRank()} F4WRD 🇺🇸🦅
-  Points: ${squadronPoints} (:small_red_triangle_down: -${pointsLost})
-  Kills: ${kills} (+${killGain})
-  Deaths: ${deaths} (+ ${deathGain})
-  K/D: ${kdRatio}
-  Players: ${squadronPlayerCount}
-  `);
-        }
-        if (squadronPoints == initialTopTenSquadPoints.squadronPoints) {
-            return;
-        }
+    
+        sendMessage(changesMessage);
+
         normalizeUpdatedData();
 
-        function calculateStats() {
-            var kdRatio = (kills / deaths).toFixed(2);
-            var killGain = kills - initialTopTenSquadPoints.kills;
-            var deathGain = deaths - initialTopTenSquadPoints.deaths;
-            var calculatedRank = () => {
-                if (rank > initialTopTenSquadPoints.rank) {
-                    return `${rank} :small_red_triangle_down: `;
-                }
-                if (rank < initialTopTenSquadPoints.rank) {
-                    return `${rank} <:smallgreenuptriangle:1083528485890445342>`;
-                } else {
-                    return rank;
-                }
-            };
-            return { killGain, deathGain, kdRatio, calculatedRank };
-        }
         function normalizeUpdatedData() {
-            initialTopTenSquadPoints.squadronPoints = squadronPoints;
-            initialTopTenSquadPoints.kills = kills;
-            initialTopTenSquadPoints.deaths = deaths;
-            initialTopTenSquadPoints.rank = rank;
+             initialTopTenSquadPoints = topTenTeamPoints;
         }
     }
-    // intervalId = setInterval(getUpdatedSquadronStats, 7 * 60 * 1000);
+     intervalId = setInterval(getUpdatedSquadronStats, 7 * 60 * 1000);
 
     async function updateGlobalVariables() {
         initialTopTenSquadPoints = await initialTopTenSquadronPoints();
-        endSessionStats = initialTopTenSquadPoints;
+        latestTopTenSquadPoints = initialTopTenSquadPoints;
     }
 };
 
